5d760fe4a43ee4020255c49b2e50d7786bad13bd
Debojyoti Mondal <97456082+debojyoti31@users.noreply.github.com>
2025-05-31 16:06:41 +0530

    Add files via upload

--- face_engine/face_model.py
+++ face_engine/face_model.py
@@ -16,10 +16,15 @@ class FaceModel:
         face = faces[0]
         return face.embedding, face.bbox
 
-    def draw_bbox(self, img, bbox, name, score=None):
+    def draw_bbox(self, img, bbox, name, threshold, score=None):
         x1, y1, x2, y2 = map(int, bbox)
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        label = f"{name}" if score is None else f"{name} ({score:.2f})"
-        cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        label = f"{name}" if score > threshold else f"{name} ({score:.2f})"
+        if score is not None and score < threshold:
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+        else:
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+       
         return img
 
